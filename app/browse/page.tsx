11849e5ae0bf93aae6eecc393dab4cd8952450a1
thebigsamm <itzsammyfavour@gmail.com>
2026-08-28 13:55:54 +0100

    Bump browse card size back up slightly (one fewer column)

--- app/browse/page.tsx
+++ app/browse/page.tsx
@@ -395,7 +395,7 @@ export default function Page() {
       ) : null}
 
       {!loading && !errorMsg ? (
-        <div className="mt-8 grid gap-4 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        <div className="mt-8 grid gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {filtered.map((p, i) => (
             <Link
               key={p.id}
